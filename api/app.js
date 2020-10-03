85d86d73fc441266bf678458f724293253ac15da
Emy_Kumba <emy@meetkumba.com>
2020-10-03 18:24:13 +0100

    added missing files

--- api/app.js
+++ api/app.js
@@ -1,32 +1,30 @@
 import express from 'express';
 import socketio from 'socket.io';
 import path from 'path';
-import db from './database/config/config';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
 
-
 export const server = app.listen(PORT, () => process.stdout.write(`Server is running on http://localhost:${PORT}/api`));
 
 const io = socketio(server);
 
 io.use(async (socket, next) => {
-    ioMiddleware(socket);
-    next();
+//   ioMiddleware(socket);
+  next();
 });
 
 app.use((req, res, next) => {
-    req.io = io;
-    next();
+  req.io = io;
+  next();
 });
 
 app.get('/api', (req, res) => {
-    res.status(200).json({
-        message: res.send('<h1>Welcome to GitStart Project API</h1>')
-    });
+  res.status(200).json({
+    message: res.send('<h1>Welcome to GitStart Project API</h1>'),
+  });
 });
 
 app.use((req, res) => res.status(404).send({ status: 404, error: res.send('Route %s not found', req.url) }));
@@ -34,4 +32,3 @@ app.use((req, res) => res.status(404).send({ status: 404, error: res.send('Route
 app.use((err, req, res) => res.status(500).send({ status: 500, error: res.send('server error') }));
 
 export default app;
-

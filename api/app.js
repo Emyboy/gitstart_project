e3556d4acd8dc722dae50275afa384c3f7e56ee5
Emy_Kumba <emy@meetkumba.com>
2020-10-03 23:27:58 +0100

    created a test evn

--- api/app.js
+++ api/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import indexRoute from './routes/index';
 import cors from 'cors';
+import bodyparser from 'body-parser';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 app.use("/api/v1", indexRoute);
 
 app.get("/api", (req, res) => {
@@ -18,8 +21,8 @@ app.get("*", (req, res) => {
   res.send("<h1>404</h1><br /><p>Route Not Found!</p>")
 });
 
-
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`)
-})
+});
 
+export default app;
---
+++ api/tests/auth.test.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import chaiHTTP from 'chai-http';
+import app from '../app';
+import dotenv from 'dotenv';
+
+chai.use(chaiHTTP);
+dotenv.config();
+
+describe('Testing auth routes', () => {
+    afterEach(done => {
+        done();
+    })
+    it('should send working from AUTH', done => {
+        chai.request(app).get(process.env.BASE_ROUTE+'/auth').end((err, res) => {
+            console.log(res.BODY);
+            done();
+        })
+    })
+
+})
+

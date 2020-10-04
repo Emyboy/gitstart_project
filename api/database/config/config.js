3035243508927ccb8bff2e593e3479381051f75b
Emy_Kumba <emy@meetkumba.com>
2020-10-04 10:58:14 +0100

    fixed dev url in test

--- api/database/config/config.js
+++ api/database/config/config.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+console.log({
+  dev: process.env.DEV_DATABASE_URL,
+  test: process.env.TEST_DATABASE_URL,
+  prod: process.env.DATABASE_URL
+})
+
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL || '',

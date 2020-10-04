ae3535a9df68fc9290c056d940612b84ea2e7669
Emy_Kumba <emy@meetkumba.com>
2020-10-05 04:58:05 +0800

    wrote first test check of CI

--- api/tests/auth.test.js
+++ api/tests/auth.test.js
@@ -1,4 +1,4 @@
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import chaiHTTP from 'chai-http';
 import app from '../app';
 import dotenv from 'dotenv';
@@ -6,13 +6,22 @@ import dotenv from 'dotenv';
 chai.use(chaiHTTP);
 dotenv.config();
 
+const mockUser = {
+    firstName: 'firstName',
+    lastName: 'lastName',
+    username: 'username',
+    password: 'password',
+    date_of_birth: '12-12-2020'
+}
+
 describe('Testing auth routes', () => {
     afterEach(done => {
         done();
     })
     it('should send working from AUTH', done => {
-        chai.request(app).get(process.env.BASE_ROUTE+'/auth').end((err, res) => {
-            // console.log(res.BODY);
+        chai.request(app).post(process.env.BASE_ROUTE+'/auth').send(mockUser).end((err, res) => {
+            expect(res.body).to.be.an('object');
+            expect(res.body).to.have.property('data');
             done();
         })
     })

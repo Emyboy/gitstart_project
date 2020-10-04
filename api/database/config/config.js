7bb0b01a172055ee505dddde64dd538aa9238755
Emy_Kumba <emy@meetkumba.com>
2020-10-04 11:06:03 +0100

    fixed dev url in test

--- api/database/config/config.js
+++ api/database/config/config.js
@@ -8,17 +8,17 @@ console.log({
 
 module.exports = {
   development: {
-    url: process.env.DEV_DATABASE_URL || '',
+    url: `${process.env.DEV_DATABASE_URL}`,
     dialect: 'postgres',
     logging: false
   },
   test: {
-    url: process.env.TEST_DATABASE_URL,
+    url: `${process.env.TEST_DATABASE_URL}`,
     dialect: 'postgres',
     logging: false
   },
   production: {
-    url: process.env.DATABASE_URL,
+    url: `${process.env.DATABASE_URL}`,
     dialect: 'postgres',
     logging: false
   },

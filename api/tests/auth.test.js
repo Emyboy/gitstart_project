4c4d87c18216b9b330cd39734c7083aad7c54e3b
Emy_Kumba <emy@meetkumba.com>
2020-10-05 18:13:46 +0100

    created register endpoint and tests

--- api/tests/auth.test.js
+++ api/tests/auth.test.js
@@ -21,12 +21,37 @@ describe('Testing auth routes', () => {
     // afterEach(done => {
     //     done();
     // })
-    it('should send working from AUTH', done => {
+    it('should register a user each time', done => {
         chai.request(app).post(process.env.BASE_ROUTE + '/auth').send(mockUser)
             .end((err, res) => {
+                expect(res.status).to.equal(201);
+                expect(res.body).to.be.an('object');
+                expect(res.body).to.have.property('data');
+                expect(res.body.data).to.be.an('object');
+                expect(res.body.data).to.have.property('message');
+                expect(res.body.data.message).to.be.a('string');
+                expect(res.body.data.message).to.equal('Signed Up');
+                expect(res.body.data).to.have.property('newUser');
+                expect(res.body.data.newUser).to.have.property('username');
+                expect(res.body.data).to.have.property('token');
+                expect(res.body.data.token).to.be.a('string');
                 done();
             })
-    })
+    });
+
+    it('should return user already exist 409', done => {
+        chai.request(app).post(process.env.BASE_ROUTE + '/auth').send(mockUser)
+            .end((err, res) => {
+                expect(res.status).to.equal(409);
+                expect(res.body).to.be.an('object');
+                expect(res.body).to.have.property('data');
+                expect(res.body.data).to.be.an('object');
+                expect(res.body.data).to.have.property('message');
+                expect(res.body.data.message).to.be.a('string');
+                expect(res.body.data.message).to.equal('Email already exist')
+                done();
+            })
+    });
 
 })
 
